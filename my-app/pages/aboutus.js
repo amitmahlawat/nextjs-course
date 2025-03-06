c1fc2fb14bcd1f9040f2155ba94ab9505ebb4c0c
amitmahlawat <amitmahlawat3@gmail.com>
2025-03-06 14:56:44 +0530

    first commit

---
+++ my-app/pages/aboutus.js
@@ -0,0 +1,11 @@
+
+
+function aboutus() {
+  return (
+    <div>
+      <h1>the about us page</h1>
+    </div>
+  )
+}
+
+export default aboutus
---
+++ my-app/pages/news.js
@@ -0,0 +1,10 @@
+
+const NewsPage=()=>{
+
+
+    return (
+        <h1>this is news page</h1>
+    )
+} 
+
+export default NewsPage;
